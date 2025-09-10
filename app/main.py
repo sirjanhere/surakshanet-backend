52a01f4300cf4feb12a95c61df797d8a364d5841
Sirjan Kaur Pahuja <kaurpahujasirjan@gmail.com>
2025-09-10 18:42:58 +0530

    Pushing main.py minimal code.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.api.v1 import crowd, face, sos, anomaly, navigation
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "SurakshaNet Backend Running!"}
+app.include_router(crowd.router, prefix="/api/v1/crowd")
+app.include_router(face.router, prefix="/api/v1/face")
+app.include_router(sos.router, prefix="/api/v1/sos")
+app.include_router(anomaly.router, prefix="/api/v1/anomaly")
+app.include_router(navigation.router, prefix="/api/v1/navigation")
